565ea5598c253e8ba76953aae4cf44a6a5417926
Douglas Leal <douglas.leal@live.com>
2023-03-21 15:49:49 -0300

    Index - completo

--- index.php
+++ index.php
@@ -2,6 +2,175 @@
 get_header();
 ?>
 
+    <section class="sobre secao">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-6">
+                    <h2 class="secao-titulo">
+                        Desenvolvedor Web
+                    </h2>
+                    <p class="secao-subtitulo">O que posso fazer por você?</p>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p>
+                        Ofereço soluções personalizadas conforme as suas necessidades. O meu trabalho é focado em
+                        desenvolver sites
+                        que ajudem profissionais liberais, pequenas e médias empresas a aumentarem sua presença no mundo
+                        digital. Se
+                        você deseja:
+                    </p>
+                    <ul class="sobre-lista">
+                        <li><i class="fas fa-caret-square-right"></i> Passar mais credibilidade</li>
+                        <li><i class="fas fa-caret-square-right"></i> Ter mais visibilidade</li>
+                        <li><i class="fas fa-caret-square-right"></i> Atingir novos clientes</li>
+                        <li><i class="fas fa-caret-square-right"></i> Melhorar a comunicação com seus clientes</li>
+                    </ul>
+                    <p>Entre em contato e solicite um orçamento sem compromisso.</p>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <a class="sobre-btn-contato btn btn-primary">Contato</a>
+                        </div>
+                        <div class="col-6">
+                            <a class="sobre-btn-wpp btn btn-primary">Whatsapp</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="servicos secao">
+        <h2 class="secao-titulo">
+            Serviços
+        </h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-4 offset-lg-2">
+                    <div class="card servico-item">
+                        <div class="card-body">
+                            <i class="servico-icone fas fa-laptop-code"></i>
+                            <h3 class="servico-titulo">Sites</h3>
+                            <p class="servico-texto">Ainda não está marcando presença na internet? Tenha um site com
+                                layout
+                                personalizado e divulgue o seu trabalho.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-4">
+                    <div class="card servico-item">
+                        <div class="card-body">
+                            <i class="servico-icone fas fa-laptop-code"></i>
+                            <h3 class="servico-titulo">Design Responsivo</h3>
+                            <p class="servico-texto">Seu site estará pronto para diversos tamanhos de tela, seja
+                                computador, tablet ou
+                                smartphone.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-4 col-lg-4 offset-lg-2">
+                    <div class="card servico-item">
+                        <div class="card-body">
+                            <i class="servico-icone fas fa-laptop-code"></i>
+                            <h3 class="servico-titulo">SEO</h3>
+                            <p class="servico-texto">Quer ser encontrado no Google? Seu site será otimizado para ser
+                                encontrado pelos
+                                sites de busca.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-4">
+                    <div class="card servico-item">
+                        <div class="card-body">
+                            <i class="servico-icone fas fa-laptop-code"></i>
+                            <h3 class="servico-titulo">Suporte e manutenção</h3>
+                            <p class="servico-texto">Precisando de ajustes, correções, atualização de recursos, trocar
+                                ou adicionar
+                                textos e imagens?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="secao portfolio">
+        <h2 class="secao-titulo">
+            Portfolio
+        </h2>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-4">
+                    <img class="img-fluid img-thumbnail" src="<?= get_template_directory_uri() . '/assets/img/background-header.jpg' ?>"
+                         alt="imagem portfolio">
+                </div>
+
+                <div class="col-sm-12 col-lg-4">
+                    <img class="img-fluid img-thumbnail" src="<?= get_template_directory_uri() . '/assets/img/background-header.jpg' ?>"
+                         alt="imagem portfolio">
+                </div>
+
+                <div class="col-sm-12 col-lg-4">
+                    <img class="img-fluid img-thumbnail" src="<?= get_template_directory_uri() . '/assets/img/background-header.jpg' ?>"
+                         alt="imagem portfolio">
+                </div>
+
+                <div class="col-sm-12 col-lg-4">
+                    <img class="img-fluid img-thumbnail" src="<?= get_template_directory_uri() . '/assets/img/background-header.jpg' ?>"
+                         alt="imagem portfolio">
+                </div>
+
+                <div class="col-sm-12 col-lg-4">
+                    <img class="img-fluid img-thumbnail" src="<?= get_template_directory_uri() . '/assets/img/background-header.jpg' ?>"
+                         alt="imagem portfolio">
+                </div>
+
+                <div class="col-sm-12 col-lg-4">
+                    <img class="img-fluid img-thumbnail" src="<?= get_template_directory_uri() . '/assets/img/background-header.jpg' ?>"
+                         alt="imagem portfolio">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contato secao">
+        <h2 class="secao-titulo">Contato</h2>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-6">
+                    <div class="contatos">
+                        <div class="contato-item">
+                            <i class="contato-icone far fa-envelope"></i>
+                            <h3 class="contato-titulo">Email</h3>
+                            <a class="contato-link" href="#">contato@douglasleal.com.br</a>
+                        </div>
+
+                        <div class="contato-item">
+                            <i class="contato-icone fab fa-whatsapp"></i>
+                            <h3 class="contato-titulo">Whatsapp</h3>
+                            <a class="contato-link" href="#">(21) 98765-4321</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <form class="formulario" action="#">
+                        <input class="form-control" type="text" placeholder="Nome*">
+
+                        <input class="form-control" type="text" placeholder="Email*">
+
+                        <input class="form-control" type="text" placeholder="DDD+Whatsapp">
+
+                        <textarea class="form-control" rows="10" placeholder="Mensagem*"></textarea>
+
+                        <input class="btn btn-outline-primary" type="submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <?php
 get_footer();
 ?>
