08b33f422f5becb25a72081f268027ac55626476
Douglas Leal <douglas.leal@live.com>
2023-04-23 13:53:55 -0300

    funcions modificado para registrar menu e navwalker

--- functions.php
+++ functions.php
@@ -7,3 +7,13 @@ function dl_scripts_e_estilos(): void
         get_template_directory_uri() . '/assets/css/style.css' );
 }
 add_action('wp_enqueue_scripts', 'dl_scripts_e_estilos');
+
+function dl_register_nav_menu(){
+    register_nav_menu('menu_principal', "Menu Principal");
+}
+add_action('after_setup_theme', 'dl_register_nav_menu');
+
+function dl_register_navwalker(){
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'dl_register_navwalker' );
